724495badb9e0a98c59d0f34dfdcbdcea1627ea3
Peter Menocal <petermenocal@pm-macbook.local>
2018-08-15 15:22:49 -0700

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <v-app dark class="black">
+      <v-content>
+        <v-container fluid fill-height class="ma-0 pa-0">
+            <router-view></router-view>
+        </v-container>
+      </v-content>
+    </v-app>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'screens'
+  }
+</script>
---
+++ src/renderer/components/WelcomeView.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-layout>
+    <v-scale-transition>
+      <img :key="active" :src="`static/slide-${active}.jpg`" alt="">
+    </v-scale-transition>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    name: 'welcome',
+    created: function () {
+      var vm = this
+      var activeIdx = 0
+      setInterval(function () {
+        activeIdx++
+        if (activeIdx > 5) {
+          activeIdx = 0
+          vm.active = 0
+        }
+        vm.active = activeIdx
+      }, 5000)
+    },
+    data () {
+      return {
+        active: 0,
+        slides: [
+          { id: 0, src: '/static/slide-0.jpg' },
+          { id: 1, src: '/static/slide-1.jpg' },
+          { id: 2, src: '/static/slide-2.jpg' },
+          { id: 3, src: '/static/slide-3.jpg' },
+          { id: 4, src: '/static/slide-4.jpg' },
+          { id: 5, src: '/static/slide-5.jpg' }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .centered
+  {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+</style>
